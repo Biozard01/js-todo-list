cdd8eb91fba246485ceea4bffd3750b0b729633e
Biozard01 <arthur.laforest33@gmail.com>
2020-11-24 10:10:09 +0100

    agva

--- api.js
+++ api.js
@@ -1,21 +1,22 @@
 const crudfulConfig = {
   headers: {
-    cfAccessKey: "75ca4ddbde8291858f35dfc4b8b75f7b45e40700",
+    cfAccessKey: "b599887d2bdc75ceaef7b06459f28e573de06d01",
   },
 };
 
 export const getTasks = (listId) =>
   axios
-    .get(
-      `https://todo.crudful.com/tasks?listId=${listId}`,
-      crudfulConfig
-    )
-    .then((result) => result.data.results);
+  .get(
+    `https://todo.crudful.com/tasks?listId=${listId}`,
+    crudfulConfig
+  )
+  .then((result) => result.data.results);
 
 export const setTaskIsCompleted = (taskId, isCompleted) =>
   axios.patch(
-    `https://todo.crudful.com/tasks/${taskId}`,
-    { isCompleted: isCompleted },
+    `https://todo.crudful.com/tasks/${taskId}`, {
+      isCompleted: isCompleted
+    },
     crudfulConfig
   );
 
@@ -27,15 +28,17 @@ export const deleteTask = (taskId) =>
 
 export const createTask = (title, listId) =>
   axios.post(
-    "https://todo.crudful.com/tasks",
-    { title: title, listId: listId },
+    "https://todo.crudful.com/tasks", {
+      title: title,
+      listId: listId
+    },
     crudfulConfig
   );
 
 export const getLists = () =>
   axios
-    .get("https://todo.crudful.com/lists", crudfulConfig)
-    .then((result) => result.data.results);
+  .get("https://todo.crudful.com/lists", crudfulConfig)
+  .then((result) => result.data.results);
 
 export const deleteList = (listId) =>
   axios.delete(
@@ -45,7 +48,8 @@ export const deleteList = (listId) =>
 
 export const createList = (title) =>
   axios.post(
-    "https://todo.crudful.com/lists",
-    { title: title },
+    "https://todo.crudful.com/lists", {
+      title: title
+    },
     crudfulConfig
   );
